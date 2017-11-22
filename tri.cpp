a85266f177e6c6c6aaef6d6883ae077c51aa5794
Pho3 <rainbowdashtheone@gmail.com>
2017-11-22 01:33:55 +0100

    tri fingerprint

--- tri.cpp
+++ tri.cpp
@@ -17,13 +17,12 @@ Table::Table(int size) {
 	Q = T[size-1];
 }
 
-Table::Table(int * Tab) {
+Table::Table(int * Tab, int s) {
 	int i=0;
 	n = 0;
-	while(Tab[i]) i++;
-	T= new int[i];
+	T= new int[s];
 	H = *T;
-	while(n<i) {
+	while(n<s) {
 		T[n]=Tab[n];
 		n++;
 	}
@@ -39,6 +38,13 @@ void Table::print() {
 	cout<<endl;
 }	
 
+void prn(int * T, int sz) {
+	while(sz>0) {
+		cout << T[sz]<< " ";
+		sz--;
+	}
+}
+
 void Table::append(int i) {
 	T[n]=i;
 	Q = T[n];
@@ -52,10 +58,98 @@ void Table::swap(int p1, int p2) {
 	T[p2]=i;
 }
 
+void Table::TBull() {
+	for(int i=0; i<n; i++) {
+		for(int j=n; j>i; j--) {
+			if (T[j]<T[j-1]) swap(j, j-1);
+		}
+	}
+}
+
+void Table::TInsr() {
+	if(n) {
+		n--;
+		TInsr();
+		n++;
+		int k = n;
+		while((k>0)&&(T[k-1]>T[k])) {
+			swap(k, k-1);
+			k--;
+		}
+	}
+}
+int * Table::Merge(int h, int q, int m) {
+	int i=h;
+	int j = m+1;
+	int R[n];
+	int k = 0;
+	while (i<=m&&j<=n) (T[i]<T[j]&&T[i]!=0)?(R[k++]=T[i++]):(R[k++]=T[j++]);
+	while(i<=m) R[k++]=T[i++];
+	while(j<=n) R[k++]=T[j++];
+	for (int i=n; i>=h; i--) T[i]=R[--k];	
+	return T;
+	
+}
+int * Table::TFus(int h, int q) {
+	if(h<q) {
+		int m = (h+q)/2;
+		T = TFus(h, m);
+		T = TFus(m+1, q);
+		T = Merge(h, q, m);
+	}
+	return T;
+}
+int * Table::Part(int h, int q, int m) {
+	int l = h+1;
+	int k = q;
+	while (l <= k) {
+		while(T[l] <= T[h]) l++;
+		while(T[k] > T[h]) k--;
+		if (l<k) swap(l++, k--);
+	}
+	m = k;
+	swap(h, k);
+	return T;
+}
+int * Table::TQui(int h, int q) {
+	for(int m=h; m<=q; m++) {
+		if(h<q) {
+			Part(h, q, m);
+			TQui(h, m-1);
+			TQui(m+1, q);
+		}
+	}
+	return T;
+}
+
+void Table::TFusn() {
+	T=TFus(0, n);
+}
+
+void Table::TQuik() {
+	T=TQui(0, n);
+}
+
+int * RFill(int n) {
+	int * T = new int[n];
+	for (int i=0; i<n; i++) T[i]=(rand() % n)+1;
+	return T;
+}
+
+void Table::Tests() {
+	int n = 0;
+	for(n=5; n<=500; n+=100) {
+		cout <<n<<"\t...\t...\t...\t...\t..."<<endl;
+	}
+}
+
+
 int main() {
 	int AT[]={4, 7, 8, 1, 9, 2};
 	Table T(12);
-	Table U(AT);
+	Table U(AT, 6);
+	Table V(RFill(9), 9);
+	V.print();
 	U.print();
 	T.append(4);
 	T.append(7);
@@ -63,8 +157,20 @@ int main() {
 	T.append(1);
 	T.append(9);
 	T.append(2);
+	T.append(5);
 	T.swap(0, 1);
 	T.print();
+	//T.TBull();
+	//T.TInsr();
+	//U.TFusn();
+	//T.TFusn();
+	T.TQuik();
+	U.TQuik();
+	V.TQuik();
+	U.print();
+	T.print();
+	V.print();
+	T.Tests();
 	return 0;
 }
 
--- tri.h
+++ tri.h
@@ -1,12 +1,22 @@
 class Table {
 	int * T, H, Q;
 	int n;
+	private:
+		int * TFus(int h, int q);
+		int * Merge(int h, int q, int m);
+		int * TQui(int h, int q);
+		int * Part(int h, int q, int m);
 	public:
 		Table();
 		Table(int size);
-		Table(int * Tab);
+		Table(int * Tab, int s);
 		void print();
 		void append(int i);
 		void swap(int p1, int p2);
+		void TBull();
+		void TInsr();
+		void TFusn();
+		void TQuik();
+		void Tests();
 		~Table();
 };
