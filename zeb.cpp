d915352a27aa6ed77b315477d502a33c4886b3c1
Pho3 <rainbowdashtheone@gmail.com>
2017-11-26 00:31:49 +0100

    zeb

--- zeb.cpp
+++ zeb.cpp
@@ -3,6 +3,62 @@
 
 using namespace std;
 
+string color(int i) {
+	switch(i) {
+		case 1: return "red"; break;
+		case 2: return "green"; break;
+		case 3: return "white"; break;
+		case 4: return "yellow"; break;
+		case 5: return "blue"; break;
+	}
+	return "NULL";
+}
+
+string drink(int i) {
+	switch(i) {
+		case 1: return "coffee"; break;
+		case 2: return "milk"; break;
+		case 3: return "wine"; break;
+		case 4: return "water"; break;
+		case 5: return "thea"; break;
+	}
+	return "NULL";
+}
+
+string pet(int i) {
+	switch(i) {
+		case 1: return "doggo"; break;
+		case 2: return "snail"; break;
+		case 3: return "fox"; break;
+		case 4: return "horse"; break;
+		case 5: return "zebra"; break;
+	}
+	return "NULL";
+}
+
+string smoke(int i) {
+	switch(i) {
+		case 1: return "OldGold"; break;
+		case 2: return "Kool"; break;
+		case 3: return "Chest"; break;
+		case 4: return "Craven"; break;
+		case 5: return "Git"; break;
+	}
+	return "NULL";
+}
+
+string natn(int i) {
+	switch(i) {
+		case 1: return "en"; break;
+		case 2: return "es"; break;
+		case 3: return "uk"; break;
+		case 4: return "no"; break;
+		case 5: return "jp"; break;
+	}
+	return "NULL";
+}
+
+
 Mat::Mat() {
 	G = new int*[5];
 	for(int i=0; i<5; i++) G[i]=new int[5];
@@ -16,6 +72,21 @@ Mat::~Mat() {
 void Mat::print() {
 	for(int i=0; i<5; i++) {
 		for(int j=0; j<5; j++) cout<<G[j][i]<< " ";
+		cout<<endl;
+	}
+}
+
+void Mat::pprint() {
+	for(int i=0; i<5; i++) {
+		for(int j=0; j<5; j++) {
+			switch(i+1) {
+				case 1: cout<<color(G[j][i])<<"\t"; break;
+				case 2: cout<<drink(G[j][i])<<"\t"; break;
+				case 3: cout<<pet(G[j][i])<<"\t"; break;
+				case 4: cout<<smoke(G[j][i])<<"\t"; break;
+				case 5: cout<<natn(G[j][i])<<"\t"; break;
+			}
+		}
 		cout<<endl;
 	}
 }
@@ -42,16 +113,6 @@ void Mat::insertxy(int **xy, int xs, int ys) {
 	for(xs; xs>0; xs--) for(ys=i; ys>0; ys--) if(xy[ys-1][xs-1]<6) G[ys-1][xs-1]=xy[ys-1][xs-1];
 }
 
-string color(int i) {
-	switch(i) {
-		case 0: return "red"; break;
-		case 1: return "green"; break;
-		case 2: return "white"; break;
-		case 3: return "yellow"; break;
-		case 4: return "blue"; break;
-	}
-}
-
 int main() {
 	Mat M;
 	int x1[5] = {1,2,3,4,5};
@@ -59,7 +120,7 @@ int main() {
 	int x3[5] = {3,4,5,1,2};
 	int x4[5] = {4,5,1,2,3};
 	int x5[5] = {5,1,2,3,4};
-	int y3[5] = {3,4,5,1,2};
+	//int y3[5] = {3,4,5,1,2};
 	int **m;
 	m=new int*[3];
 	for(int i=0; i<3; i++) m[i]=new int[3];
@@ -76,17 +137,103 @@ int main() {
 		for(int j=0; j<3; j++) cout << m[j][i]<< " ";
 		cout << endl;
 	}
-	M.insertx(x1, 1, 5);
-	M.insertx(x2, 2, 5);
-	M.insertx(x4, 4, 5);
-	M.insertx(x3, 3, 5);
-	M.insertx(x5, 5, 5);
+	// M.insertx(x1, 1, 5);
+	// M.insertx(x2, 2, 5);
+	// M.insertx(x4, 4, 5);
+	// M.insertx(x3, 3, 5);
+	// M.insertx(x5, 5, 5);
 
-	M.inserty(3, y3, 4);
-	M.print();
-	cout << endl;
-	M.insertxy(m, 3, 3);
-	M.print();
+	//M.inserty(3, y3, 4);
+	// M.print();
+	// cout << endl;
+	// M.insertxy(m, 3, 3);
+	// M.print();
+	//M.pprint();
 	//cout << endl << color(3) << drink(2) << pet(4) << smoke(5) << natn(1)<<endl;
+
+	/* Init Infos */
+
+	/* linear */
+	int l1[5] = {1,6,6,6,1}; // !c1			!l3	!l4	l5	!l6	l7
+	//int l2[5] = {6,6,1,6,2};
+	int l3[5] = {3,1,6,6,6}; // !c1 !l1			!l4	l5	l6	!l7
+	int l4[5] = {6,5,6,6,3}; // !c1 !l1	!l3			l5	!l6	!l7
+	int l5[5] = {6,6,2,1,6}; // !c1 l1	l3	l4	l5	!l6	!l7
+	int l6[5] = {6,6,6,4,5}; // !c1 !l1	l3	!l4	!l5			!l7
+	int l7[5] = {6,3,6,5,6}; // !c1 l1	!l3	!l4	!l5	!l6
+	//int l8[5] = {5,6,6,6,2};
+	//int l9[5] = {5,6,6,2,6};
+
+	int a1[5] = {6,6,6,6,4}; // absolute condition
+	int a2[2] = {6,2};
+
+	/* floating condition 1 */
+
+	int **f1;
+	f1=new int*[3];
+	for(int i=0; i<3; i++) f1[i]=new int[5];
+	for(int i=0; i<5; i++) for(int j=0; j<3; j++) f1[j][i]=6;
+
+	f1[0][2]=3;
+	f1[2][2]=3;
+	f1[1][3]=3;
+
+	/* floating condition 2 */
+
+	int **f2;
+	f2=new int*[3];
+	for(int i=0; i<3; i++) f2[i]=new int[5];
+	for(int i=0; i<5; i++) for(int j=0; j<3; j++) f2[j][i]=6;
+
+	f2[0][2]=4;
+	f2[2][2]=4;
+	f2[1][3]=2;
+
+
+	/* floating condition 3 */
+
+	int **f3;
+	f3=new int*[3];
+	for(int i=0; i<3; i++) f3[i]=new int[5];
+	for(int i=0; i<5; i++) for(int j=0; j<3; j++) f3[j][i]=6;
+
+	f3[0][2]=4;
+	f3[2][2]=4;
+	f3[1][3]=2;
+
+	// for(int i=0; i<5; i++) {
+	// 	for(int j=0; j<3; j++) cout << f1[j][i]<< " ";
+	// 	cout << endl;
+	// }
+
+	/* compressed condition */
+	int c1[5] = {5,6,1,2,2}; // l2 + l8 + l9
+
+	/* potential compressed condition */
+	int c2[5] = {1,3,0,5,1}; // l1 + l7
+	int c3[5] = {3,1,2,1,0}; // l3 + l5
+	int c4[5] = {0,5,2,1,3}; // l4 + l5
+	int c5[5] = {1,0,2,1,1}; // l1 + l5
+	int c6[5] = {3,1,0,4,5}; // l3 + l6
+
+	/* potential solutions groups */
+	// g1 = c1 + c2 + c3 + l4 + l6
+	int g1_1[5] = {5,0,1,2,2};
+	int g1_2[5] = {1,3,0,5,1};
+	int g1_3[5] = {3,1,2,1,0};
+	int g1_4[5] = {6,5,6,6,3};
+	int g1_5[5] = {6,6,6,4,5};
+	// g2 = c1 + c2 + c4 + c6
+	int g2_1[5] = {1,3,0,5,1};
+	int g2_2[5] = {0,5,2,1,3};
+	int g2_3[5] = {3,1,0,4,5};
+	int g2_4[5] = {5,0,1,2,2};
+
+
+	M.insertx(a1, 1, 5);
+	M.inserty(2, a2, 3);
+
+	M.print();
+
 	return 0;
 }
--- zeb.h
+++ zeb.h
@@ -1,17 +1,15 @@
 #include <cstring>
 #include <string>
 using namespace std;
-/*
 string color(int i);
 string drink(int i);
 string pet(int i);
 string smoke(int i);
 string natn(int i);
-*/
 /*{ red, blue, green, white, yellow };
-enum drink{ coffee, milk, wine, wather, thea };
+enum drink{ coffee, milk, wine, water, thea };
 enum pet{ doggo, snail, fox, horse, zebra };
-enum smoke{ OldGold, Kool, Chest, Caven, Git};
+enum smoke{ OldGold, Kool, Chest, Craven, Git};
 enum natn{ en, es, uk, no, jp};
 */
 
@@ -21,6 +19,7 @@ class Mat {
 		Mat();
 		~Mat();
 		void print();
+		void pprint();
 		void insert(int x, int y, int v);
 		void insertx(int * x, int y, int sz);
 		void inserty(int x, int *y, int sz);
